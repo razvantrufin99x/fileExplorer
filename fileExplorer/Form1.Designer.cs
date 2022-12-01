19dc830633f586aa1960811504c2bfd297a75e13
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 23:19:53 +0200

    Add project files.

---
+++ fileExplorer/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace fileExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(59, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(191, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Drive:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(256, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse selected Drive";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(23, 43);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(191, 388);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.VirtualListSize = 5;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(220, 43);
+            this.listView2.MultiSelect = false;
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(347, 388);
+            this.listView2.TabIndex = 4;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            this.listView2.VirtualListSize = 5;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(567, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(20, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(2, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(23, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(2, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(23, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = ">";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 455);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "files Explorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ fileExplorer/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace fileExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string currentpath = "";
+        public string currentdir = "";
+       
+        public List<string> stackpathdirs = new List<string>();
+
+        public void createpath()
+        {
+            //create a path from list stackpathdirs and print in title of form
+            Text = "";
+            for (int i = 0; i < stackpathdirs.Count; i++)
+            {
+                
+                Text += stackpathdirs[i].ToString();
+                currentdir += stackpathdirs[i].ToString();
+            }
+             
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //find all logical drivers
+
+           string [] logicaldrives =  Environment.GetLogicalDrives();
+            //add all logical drivers in combobox1 for selection
+           for (int i = 0; i < logicaldrives.Length; i++)
+           {
+               comboBox1.Items.Add(logicaldrives[i].ToString());
+           }
+            
+            //select the text selected from combobox1 and add this is currentpath and in title of form
+            comboBox1.SelectedItem= comboBox1.Items[0];
+            comboBox1.SelectAll();
+
+            currentpath = comboBox1.SelectedText;
+            Text = currentpath;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //search all dirs form current logical drive
+            //and addthem all of the in listview1
+            DirectoryInfo di = new DirectoryInfo(currentpath);
+            DirectoryInfo[] directories = di.GetDirectories();
+            for (int i = 0; i < directories.Length; i++)
+           {
+               listView1.Items.Add(directories[i].ToString());
+           }
+          
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //select all text from combobox1
+            //add text of combobox1 in current path and the nto title of form
+            comboBox1.SelectAll();
+            currentpath = comboBox1.SelectedText;
+            Text = currentpath;
+          
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+
+            
+
+           
+       
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+
+            //find selected item
+            ListViewItem item0 = listView2.SelectedItems[0];
+
+             //updatepath
+            //find path of selected intem
+            ListViewItem item02 = listView2.SelectedItems[0];
+
+            currentdir += "\\" +  item02.Text;
+
+            Text = currentpath  + currentdir;
+
+
+            //remove all from listaview2
+            for (int i = listView2.Items.Count - 1; i >= 0; i--)
+            {
+                try
+                {
+                    ListViewItem item1 = listView2.Items[i];
+                    listView2.Items.Remove(item1);
+                }
+                catch { }
+            }
+
+            //remove all from listaview1
+            for (int i = listView1.Items.Count - 1; i >= 0; i--)
+            {
+                try
+                {
+                    ListViewItem item2 = listView1.Items[i];
+                    listView1.Items.Remove(item2);
+                }
+                catch { }
+            }
+
+            //add item0 in listview1
+            listView1.Items.Add(item0);
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //remove all items from listview2
+            for (int i = listView2.Items.Count - 1; i >= 0; i--)
+            {
+                try
+                {
+                    ListViewItem item0 = listView2.Items[i];
+                    listView2.Items.Remove(item0);
+                }
+                catch { }
+            }
+
+            try
+            {
+                //find path of selected intem
+                ListViewItem item = listView1.SelectedItems[0];
+
+                currentdir += "\\" + item.Text;
+
+                Text = currentpath  + currentdir;
+
+
+
+                // add all subdirectories of current directory on listview2
+                DirectoryInfo di = new DirectoryInfo(currentpath + currentdir);
+                DirectoryInfo[] directories = di.GetDirectories();
+                for (int i = 0; i < directories.Length; i++)
+                {
+                    listView2.Items.Add(directories[i].ToString());
+                }
+            }
+            catch { }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // add all subdirectories of current directory on listview2
+            DirectoryInfo di = new DirectoryInfo(Text);
+            DirectoryInfo[] directories = di.GetDirectories();
+            for (int i = 0; i < directories.Length; i++)
+            {
+                listView2.Items.Add(directories[i].ToString());
+            }
+        }
+    }
+}
